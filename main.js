ee22c05a53b3ed3edd704d01f8d90a81cde6d74a
Santhosh Rajendran <139849562+Santhosh-1703@users.noreply.github.com>
2024-07-14 14:58:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text",{
+    strings:['Customer Service Associate.','Business Intelligence.','Meme Creation.','Food Vlogging.','Adventure Explorer.','Data Analytics.'],
+    typeSpeed:150,
+    backSpeed:150,
+    backDelay:1000,
+    loop: true
+})
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
