6e0d682377a891a9f46d43ad66fb7561ea7e4969
Santhosh Rajendran <139849562+Santhosh-1703@users.noreply.github.com>
2024-11-18 15:44:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:['Customer Service Associate.','Business Intelligence.','Meme Creation.','Food Vlogging.','Adventure Explorer.','Data Analytics.'],
+    strings:['Customer Services & Operations.','Business Intelligence.','Meme Creation.','Food Vlogging.','Adventure Explorer.','Data Analytics.'],
     typeSpeed:150,
     backSpeed:150,
     backDelay:1000,
